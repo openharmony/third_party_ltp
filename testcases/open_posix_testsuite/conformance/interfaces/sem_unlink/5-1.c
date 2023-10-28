dbeb4d8a7800ec8f5c4c52e867140727288216e2
zWX1144765 <zhaominjie9@huawei.com>
2023-10-28 18:25:36 +0800

    修改sem信号传入na_max值为256
    Signed-off-by: zWX1144765 <zhaominjie9@huawei.com>

--- testcases/open_posix_testsuite/conformance/interfaces/sem_unlink/5-1.c
+++ testcases/open_posix_testsuite/conformance/interfaces/sem_unlink/5-1.c
@@ -166,7 +166,7 @@ int main(void)
 
 	if (NAME_MAX > 0) {
 		/* create a semaphore with a name longer than NAME_MAX */
-		sem_name = calloc(NAME_MAX + 2, sizeof(char));
+		sem_name = calloc(NAME_MAX + 3, sizeof(char));
 
 		if (sem_name == NULL) {
 			UNRESOLVED(errno,
@@ -176,9 +176,9 @@ int main(void)
 		/* the space was allocated */
 		sem_name[0] = '/';
 
-		sem_name[NAME_MAX + 1] = '\0';
+		sem_name[NAME_MAX + 2] = '\0';
 
-		memset(sem_name + 1, 'N', NAME_MAX);
+		memset(sem_name + 1, 'N', NAME_MAX + 1);
 
 		/* Create the semaphore */
 		sem = sem_open(sem_name, O_CREAT, 0777, 1);
